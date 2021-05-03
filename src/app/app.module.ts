7f3a780700becff786ca90e1876a0974dad27f09
Okinz <steven14812@gmail.com>
2021-05-03 11:32:33 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavComponent } from './components/nav/nav.component';
+import { SpeciesListComponent } from './components/species-list/species-list.component';
 
 @NgModule({
   declarations: [
     LandingPageComponent,
-    NavComponent
+    NavComponent,
+    SpeciesListComponent,
   ],
   imports: [
     AppRoutingModule,
