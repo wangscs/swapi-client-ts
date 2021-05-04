aa3ea1fcaf89ba2ef080abe477627c22cc1c0031
Okinz <steven14812@gmail.com>
2021-05-04 15:26:51 -0400

    updated species service test locations

--- src/app/tests/species.service.spec.ts
+++ src/app/tests/species.service.spec.ts
@@ -1,18 +1,17 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
-import species from 'src/app/tests/data/Species.json';
-
 import { SpeciesService } from '../services/species.service';
+import species from '../tests/data/Species.json';
+
 
 describe('SpeciesService', () => {
   let service: SpeciesService;
-  let httpClientSpy: {get: jasmine.Spy};
+  let httpClientSpy: { get: jasmine.Spy };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientModule ],
-      providers: [ HttpClient ],
+      imports: [HttpClientTestingModule]
     });
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
     service = new SpeciesService(httpClientSpy as any);
@@ -22,21 +21,19 @@ describe('SpeciesService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should get all species objects', () => {
+  it('should get all species', () => {
     httpClientSpy.get.and.returnValue(of(species));
     service.getAll().subscribe(data => {
-      expect(data.results[0].name).toContain('Human');
-      expect(data.results[1].name).toContain('Driod');
-      expect(data.results[2].name).toContain('Wookie');
+      expect(data.results[1]).toEqual(species.results[1]);
+      expect(data.results.length).toEqual(3);
     });
   });
 
   it('should get a species by id', () => {
-    httpClientSpy.get.and.returnValue(of(species[0]));
-    service.getById(1).subscribe(data => {
-      expect(data.name).toContain('Human');
-      expect(data.classification).toContain('mammal');
-      expect(data.designation).toContain('sentient');
+    let index = 1;
+    httpClientSpy.get.and.returnValue(of(species.results[index]));
+    service.getById(index).subscribe(data => {
+      expect(data).toEqual(species.results[index]);
     });
   });
 });
